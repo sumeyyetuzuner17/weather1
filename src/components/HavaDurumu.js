a73dc61473e0647f04ca8321505c8103f36563b4
sumeyyetuzuner17 <sumeyyedeniz34@gmail.com>
2023-03-29 00:01:22 +0300

    css'ler düzeltildi.

--- src/components/HavaDurumu.js
+++ src/components/HavaDurumu.js
@@ -6,12 +6,11 @@ const HavaDurumu = (props) => {
 
   if (!weather) {
     return <p>Lütfen Bekleyin...</p>;
-  }
+  }    
 
   return (
     <div className="havadurumu">
       <br />
-      {/* <h1 className="baslik">HAVA DURUMU</h1> */}
       <div className="konum">
         <h2>{weather.name}</h2>
       </div>
@@ -19,16 +18,25 @@ const HavaDurumu = (props) => {
         {new Date(weather.dt * 1000).toLocaleDateString()}
       </div>{" "}
       <br />
-      
       <div className="derece">{weather.main.temp}°</div>
-      
       <div className="aciklama">
-        <h3>"{weather.weather.map((data) => data.description).join(",")}"</h3>
+        <h3>{weather.weather.map((data) => data.description).join(",")}</h3>
+      </div>
+      <div>
+        {console.log(weather.weather.map((data) => data.main).join(","))}
       </div>
       <div className="gorsel">
-        <div className="nem">Nem <br/>{weather.main.humidity} </div>
-        <div className="rüzgar">Rüzgar <br/> {weather.wind.speed}</div>
-        <div className="basinc">Basınç <br/>{weather.main.pressure}</div>
+        <div className="nem">
+          Nem <br />
+          {weather.main.humidity}{" "}
+        </div>
+        <div className="rüzgar">
+          Rüzgar <br /> {weather.wind.speed}
+        </div>
+        <div className="basinc">
+          Basınç <br />
+          {weather.main.pressure}
+        </div>
       </div>
     </div>
   );
